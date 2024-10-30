563cb1cb7dd6443e951e0847ffdc925cb46cfaf0
tanlye <tan_lovell@hotmail.com>
2024-10-30 20:53:03 +0800

    skip checkov checks v2

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,9 @@ resource "aws_s3_bucket" "bucket" {
     #checkov:skip=CKV_AWS_145: Ensure that S3 buckets are encrypted with KMS by default
     #checkov:skip=CKV_AWS_21: Ensure all data stored in the S3 bucket have versioning enabled
     #checkov:skip=CKV_AWS_61: Ensure that an S3 bucket has a lifecycle configuration
+    #checkov:skip=CKV2_AWS_62: Ensure S3 buckets should have event notifications enabled
+    #checkov:skip=CKV2_AWS_6: Ensure that S3 bucket has a Public Access block
+    #checkov:skip=CKV2_AWS_61: Ensure that an S3 bucket has a lifecycle configuration
 }
 
 # resource "aws_sns_topic" "bucket_notifications" {
